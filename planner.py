9bf2ed11732a56982d887b7401423bdefeb709bc
aezeroth <aez82014@gmail.com>
2022-04-20 01:40:16 -0700

    Refactored

--- planner.py
+++ planner.py
@@ -1,34 +1,61 @@
 import json
 import os
 
-import units
+from units import *
 import user_menu
+
+
+sample = {
+    'Mapo Tofu' : { 
+        'ingredients' : {'tofu' : (1, Units.PACKS),
+                        'ground pork' : (500, Units.GRAMS) },
+        'meal types' : [MealType.LUNCH, MealType.DINNER]
+    }
+}
+
 
 def __init__():
     return
 
-def main():
-    sample = {
-        'name' : 'Mapo Tofu',
-        'ingredients' : {'tofu' : (1, units.PACKS),
-                         'ground pork' : (500, units.GRAMS) }
-    }
 
+def add_recipe(recipes):
+    name = input("Recipe name:")
+    ingredients = {}
+
+
+def collect_data():
     recipes = {}
 
     try:
         # read json recipe book from file
         with open('recipes.json', 'r',) as recipe_book:
             recipes = json.load(recipe_book)
+
     except json.JSONDecodeError:
         print('Valid .json file not found. Continuing wtihout reading...')
 
-    # user does stuff
+    return recipes
+
+
+def user_phase(recipes):
     print(user_menu.START)
 
+
+def write_data(recipes):
     # write json recipe back to file
     with open('recipes.json', 'w') as recipe_book:
         json.dump(sample, recipe_book, indent=4)
+
+
+
+
+def main():
+    recipes = collect_data()
+
+    # user does stuff
+    user_phase(recipes)
+
+    write_data(recipes)
 
     input('Press ENTER to quit:')
 
--- units.py
+++ units.py
@@ -1,2 +1,10 @@
-PACKS = 'packs'
-GRAMS = 'grams'
+from enum import Enum, auto
+
+class Units:
+    PACKS = 'packs'
+    GRAMS = 'grams'
+
+class MealType():
+    BREAKFAST = 'breakfast'
+    LUNCH = 'lunch'
+    DINNER = 'dinner'
